808df137bdf06a200d553aa462f61f4ba07b64e1
PornsawanK <mushroom.gead@gmail.com>
2025-02-03 13:37:06 +0700

    show ua

--- app/url/Form.tsx
+++ app/url/Form.tsx
@@ -1,4 +1,9 @@
 "use client";
 export default function Form() {
-  return <p>url: {window.location.toString()}</p>;
+  return (
+    <div>
+      <p>userAgent: {window.navigator.userAgent}</p>
+      <p>url: {window.location.toString()}</p>
+    </div>
+  );
 }
